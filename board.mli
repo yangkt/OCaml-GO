f4b2652c8880878fe37ebef10abc413b07158a5f
ky289 <ky289@cornell.edu>
2018-05-11 22:49:32 -0400

    merge fix

--- board.mli
+++ board.mli
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 (* represents the board for go*)
 type board = {
@@ -21,61 +19,7 @@ val pass : board -> board
  * be placed at board initiation *)
 val initiate_game : int -> int -> board
 
-(* [get_pos] returns a list of positions of the pieces of a given color
- * returns empty list of no pieces are on the board *)
-val get_pos : board -> int -> (int*int) list
-
-(* [place] places a piece into [(int*int)] for the given [player]
- * int int pair represents (row, column)
- * if the placement is invalid in any way, the original board is returned with
- * a descriptive message in the msg field.  *)
-val place : board -> (int*int) -> board
-
-(* [territory_score brd plr] calculates the score from purely territory on the
- * board [brd] for player [plr] *)
-val territory_score : board -> int -> int
-
-(* [stone_score brd plr] calculates the score from purely placed stones on the
- * board [brd] for player [plr] *)
-val stone_score : board -> int -> int
-
-(* [score] returns the current score for the given [player] *)
-val score : board -> int -> int
-
-(* [take_turn] lets the [player] take their turn
- * returns an updated board with the move implemented
-val take_turn : int -> move -> t *)
-
-(* returns a string representation of the board
- * used for ascii printing  *)
-val board_to_string : board -> string
-
-(* models the set in Arrays module in OCaml but does not return unit*)
-val assign : int -> int -> int -> int array array -> int array array
-
-(* [copy_matrix m] returns a deep copy of matrix [m]*)
-val copy_matrix : int array array -> int array array
-
-val get_group : int array array -> int * int -> int * (int * int) list
-=======
-
-
-(* represents the board for go*)
-type board = {
-  player : int;
-  board : int array array;
-  msg : string
-}
-
-
-(* [not_full] checks that [brd] is not full with stones *)
-val not_full : board -> bool
-
-(* val pass : board -> board *)
-
 val create : int -> int array array
-
-val pass : board -> board
 
 
 (* [initiate_game n] initiates the game.
@@ -84,8 +28,6 @@ val pass : board -> board
  *  *)
 val initiate_game : int -> int -> board
 
-
-
 (* [get_pos] returns a list of positions of the pieces of a given color
  * returns empty list of no pieces are on the board *)
 val get_pos : board -> int -> (int*int) list
@@ -122,4 +64,3 @@ val assign : int -> int -> int -> int array array -> int array array
 val copy_matrix : int array array -> int array array
 
 val get_group : int array array -> int * int -> int * (int * int) list
->>>>>>> 20bd14ee17082fb5fa984622479c556ec9e6e95e
